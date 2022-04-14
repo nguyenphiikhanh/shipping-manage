cc6759254d9b570be72f16c566309709cf82fc5f
Nguyen Phi Khanh <nguyenphiikhanh@gmail.com>
2022-04-15 01:12:43 +0700

    login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dang-nhap', 'LoginController@index')->name('login');
+
+
